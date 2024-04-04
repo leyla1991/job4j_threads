31975ebebdf7abc01d5d9db5af1c00999ffa053b
leyla1991 <atagleyla@mail.ru>
2024-04-04 18:01:37 +0300

    3.4. Временное хранение. git stash. [#504832]

--- src/main/java/ru/job4j/print/PrintHello.java
+++ src/main/java/ru/job4j/print/PrintHello.java
@@ -1,7 +1,9 @@
 package ru.job4j.print;
 
 public class PrintHello {
+
     public static void main(String[] args) {
+        System.out.println("first message");
         System.out.println("Hello? my friend");
     }
 }
