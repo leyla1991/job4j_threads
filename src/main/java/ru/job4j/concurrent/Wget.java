545c7ab1ca8bea9d6273e4212ef74adb504e5510
leyla1991 <atagleyla@mail.ru>
2024-04-07 23:04:08 +0300

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                    for (int i = 0; i <= 100; i++) {
+                        try {
+                            System.out.print("\rLoading : " + i  + "%");
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.fillInStackTrace();
+                        }
+                    }
+                });
+        first.start();
+    }
+}
