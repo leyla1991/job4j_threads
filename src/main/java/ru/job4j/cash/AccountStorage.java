f86d2e95b46ea1c69283a3f3bd4e992c7b0429ef
leyla1991 <atagleyla@mail.ru>
2024-04-28 14:23:40 +0300

    3. Денежные переводы AccountStorage [#1104]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -7,21 +7,11 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        boolean rsl = false;
-        if (!accounts.containsKey(account.id())) {
-            accounts.put(account.id(), account);
-            rsl = true;
-        }
-        return rsl;
+        return accounts.putIfAbsent(account.id(), account) != null;
     }
 
     public synchronized boolean update(Account account) {
-        boolean rsl = false;
-        if (accounts.containsKey(account.id())) {
-                accounts.put(account.id(), account);
-                rsl = true;
-            }
-        return rsl;
+        return accounts.replace(account.id(), account) != null;
         }
 
     public synchronized void delete(int id) {
@@ -36,10 +26,12 @@ public class AccountStorage {
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
         boolean rsl = false;
-        if (accounts.containsKey(fromId) && accounts.containsKey(toId)) {
-            if (accounts.get(fromId).amount() - amount >= 0) {
-                accounts.put(fromId, new Account(fromId, (accounts.get(fromId).amount() - amount)));
-                accounts.put(toId, new Account(toId, (accounts.get(toId).amount() + amount)));
+        if (getById(fromId).isPresent() && getById(toId).isPresent()) {
+            Account accountFrom = getById(fromId).get();
+            Account accountTo = getById(toId).get();
+            if (accountFrom.amount() - amount >= 0) {
+                accounts.put(fromId, new Account(fromId, (accountFrom.amount() - amount)));
+                accounts.put(toId, new Account(toId, (accountTo.amount() + amount)));
                 rsl = true;
             }
         }
