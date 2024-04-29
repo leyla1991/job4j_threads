f11f5d8ed20d5ca9b777a8c9ac7d8151f8337464
leyla1991 <atagleyla@mail.ru>
2024-04-29 17:50:34 +0300

    2. Обеспечить остановку потребителя. [#66825]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -10,7 +10,7 @@ import java.util.Queue;
 public class SimpleBlockingQueue<T> {
 
     @GuardedBy("this")
-    private Queue<T> queue = new LinkedList<>();
+    private final Queue<T> queue = new LinkedList<>();
     private final int limit;
 
     public SimpleBlockingQueue(int limit) {
