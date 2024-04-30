41ec40da1302495c2b7fad45fbe1cc67e4f198d4
leyla1991 <atagleyla@mail.ru>
2024-04-30 10:25:57 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicInteger count = new AtomicInteger();
+
+    public void increment() {
+        int rsl;
+        int exp;
+        do {
+            exp = get();
+            rsl = exp + 1;
+        } while (!count.compareAndSet(exp, rsl));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
