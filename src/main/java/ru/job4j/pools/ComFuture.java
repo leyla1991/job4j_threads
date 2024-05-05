ea6b04993a63059c9d433f75106109f5427da80e
leyla1991 <atagleyla@mail.ru>
2024-05-06 00:40:57 +0300

    4. CompletableFuture [#361626]

---
+++ src/main/java/ru/job4j/pools/ComFuture.java
@@ -0,0 +1,106 @@
+package ru.job4j.pools;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class ComFuture {
+
+    public static CompletableFuture<Void> washHands(String name) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + ", моет руки");
+        });
+    }
+
+    public static CompletableFuture<String> whoWashHands(String name) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return name + ", моет руки";
+        });
+    }
+
+    private static void iWork() throws InterruptedException {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Вы: Я работаю");
+            TimeUnit.SECONDS.sleep(1);
+            count++;
+        }
+    }
+
+    public static CompletableFuture<String> buyProduct(String product) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел в магазин");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я купил " + product);
+                    return product;
+                }
+        );
+    }
+
+    public static CompletableFuture<Void> goToTrash() {
+        return CompletableFuture.runAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел выносить мусор");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я вернулся!");
+                }
+        );
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+         CompletableFuture<Void> c = goToTrash();
+        c.thenRun(() -> {
+            int count = 0;
+            while (count < 3) {
+                System.out.println("Сын: я мою руки");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                count++;
+            }
+            System.out.println("Сын: Я помыл руки");
+        });
+
+        CompletableFuture<Void> all = CompletableFuture.allOf(
+                washHands("Папа"), washHands("Мама"),
+                washHands("Ваня"), washHands("Боря")
+        );
+        TimeUnit.SECONDS.sleep(3);
+
+        CompletableFuture<Object> first = CompletableFuture.anyOf(
+                whoWashHands("Папа"), whoWashHands("Мама"),
+                whoWashHands("Ваня"), whoWashHands("Боря")
+        );
+        System.out.println("Кто сейчас моет руки?");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(first.get());
+
+        CompletableFuture<String> sun = buyProduct("Brad").thenCombine(buyProduct("Milk"), (f, s) -> "Buy " + f + " and " + s);
+         CompletableFuture<String> s = buyProduct("milk")
+                 .thenApply((product) -> "Сын: я налил тебе в кружку " + product + ". Держи.");
+         iWork();
+        System.out.println(s.get());
+        System.out.println(sun.get());
+    }
+}
---
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -0,0 +1,83 @@
+package ru.job4j.pools;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class RolColSum {
+
+    public static class Sums {
+
+        private int rowSum;
+        private int colSum;
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+    }
+
+    public static Sums[] sum(int[][] matrix) {
+        int n = matrix.length;
+        Sums[] sums = new Sums[n];
+        for (int k = 0; k < n; k++) {
+            Sums sum = new Sums();
+            for (int z = 0; z < n; z++) {
+                sum.setRowSum(sum.getRowSum() + matrix[k][z]);
+                sum.setColSum(sum.getColSum() + matrix[z][k]);
+            }
+            sums[k] = sum;
+        }
+        return sums;
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        int n = matrix.length;
+        Sums[] sums = new Sums[n];
+        Map<Integer, CompletableFuture<Sums>> futureMap = new HashMap<>();
+        for (int k = 0; k < n; k++) {
+            futureMap.put(k, getTask(matrix, n, k));
+        }
+        for (Integer key : futureMap.keySet()) {
+            sums[key] = futureMap.get(key).get();
+        }
+        return sums;
+    }
+
+    public static CompletableFuture<Sums> getTask(int[][] matrix, int endRow, int startCol) {
+        return CompletableFuture.supplyAsync(() -> {
+            Sums sums = new Sums();
+            for (int i = 0; i < endRow; i++) {
+                    sums.setRowSum(sums.getRowSum() + matrix[startCol][i]);
+                    sums.setColSum(sums.getColSum() + matrix[i][startCol]);
+                }
+            return sums;
+        });
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Sums[] sums1 = asyncSum(matrix);
+        for (Sums sums : sums1) {
+            System.out.println("Сумма столбца: " + sums.getColSum()
+                    + " Сумма строки: " + sums.getRowSum());
+        }
+        Sums[] sums = sum(matrix);
+        for (Sums s : sums) {
+            System.out.println("Сумма столбца: " + s.getColSum()
+                    + " Сумма строки: " + s.getRowSum());
+        }
+    }
+}
